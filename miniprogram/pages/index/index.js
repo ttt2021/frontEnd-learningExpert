64b97fd82e435dfcb4041691bd9b8f526127551f
ttt2021 <570308307@qq.com>
2020-12-04 18:05:00 +0800

    learning-expert

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,7 +5,45 @@ Page({
      * 页面的初始数据
      */
     data: {
+        value: '',
+        indicatorActiveColor: "#ffffff",
+        indicatorColor: "rgba(0, 0, 0, .3)",
+        indicatorDots: true,
+        autoplay: true,
+        duration: 500,
+        interval: 3000,
+        circular: true,
+        img: [
+            {
+                image: '../../images/1.jpg', 
+                title: 'html有哪些特性'
+            },
+            {
+                image: '../../images/2.jpg', 
+                title: 'promise有没有解决异步问题'
+            },
+            {
+                image: '../../images/3.jpg', 
+                title: 'css伪类与伪元素的区别'
+            },
+            {
+                image: '../../images/4.jpg', 
+                title: '说一下事件循环机制'
+            },
+            {
+                image: '../../images/5.jpg', 
+                title: '计算属性和普通属性有什么区别'
+            }
+        ]
+    },
 
+    onChange(e) {
+        this.setData({
+          value: e.detail,
+        });
+    },
+    onSearch() {
+        console.log(this.data.value)
     },
 
     /**
